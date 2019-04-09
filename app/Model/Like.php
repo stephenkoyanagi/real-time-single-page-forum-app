df4afa1aec7bb9f846d461dcf34cc6456d184f3c
stephenkoyanagi <stephenkoyanagi@shaw.ca>
2019-04-09 13:49:53 -0600

    Like and Dislike system for reply

--- app/Model/Like.php
+++ app/Model/Like.php
@@ -10,13 +10,4 @@ class Like extends Model
 {
 	protected $guarded = [];
 
-    public function user()
-    {
-    	return $this->belongsTo(User::class);
-    }
-
-    public function reply()
-    {
-    	return $this->belongsTo(Reply::class);
-    }
 }
